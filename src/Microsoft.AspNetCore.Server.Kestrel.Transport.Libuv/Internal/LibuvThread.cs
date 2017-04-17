d2d14360cd48be1714e61e38f827110834dbba1c
Stephen Halter <halter73@gmail.com>
2017-04-17 15:12:52 -0700

    Move shutdown logic from transport to core

--- src/Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv/Internal/LibuvThread.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv/Internal/LibuvThread.cs
@@ -37,14 +37,12 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
         private bool _initCompleted = false;
         private ExceptionDispatchInfo _closeError;
         private readonly ILibuvTrace _log;
-        private readonly TimeSpan _shutdownTimeout;
 
         public LibuvThread(LibuvTransport transport)
         {
             _transport = transport;
             _appLifetime = transport.AppLifetime;
             _log = transport.Log;
-            _shutdownTimeout = transport.TransportOptions.ShutdownTimeout;
             _loop = new UvLoopHandle(_log);
             _post = new UvAsyncHandle(_log);
             _thread = new Thread(ThreadStart);
@@ -58,7 +56,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
             QueueCloseAsyncHandle = EnqueueCloseHandle;
             PipeFactory = new PipeFactory();
             WriteReqPool = new WriteReqPool(this, _log);
-            ConnectionManager = new LibuvConnectionManager(this);
         }
 
         // For testing
@@ -72,8 +69,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
 
         public PipeFactory PipeFactory { get; }
 
-        public LibuvConnectionManager ConnectionManager { get; }
-
         public WriteReqPool WriteReqPool { get; }
 
         public ExceptionDispatchInfo FatalError { get { return _closeError; } }
@@ -81,9 +76,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
         public Action<Action<IntPtr>, IntPtr> QueueCloseHandle { get; }
 
         private Action<Action<IntPtr>, IntPtr> QueueCloseAsyncHandle { get; }
-
-        // The cached result of Loop.Now() which is a timestamp in milliseconds
-        private long Now { get; set; }
 
         public Task StartAsync()
         {
@@ -104,10 +96,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
 
             if (!_threadTcs.Task.IsCompleted)
             {
-                // These operations need to run on the libuv thread so it only makes
-                // sense to attempt execution if it's still running
-                await DisposeConnectionsAsync().ConfigureAwait(false);
-
                 var stepTimeout = TimeSpan.FromTicks(timeout.Ticks / 3);
 
                 Post(t => t.AllowStop());
@@ -138,20 +126,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
             if (_closeError != null)
             {
                 _closeError.Throw();
-            }
-        }
-
-        private async Task DisposeConnectionsAsync()
-        {
-            // Close and wait for all connections
-            if (!await ConnectionManager.WalkConnectionsAndCloseAsync(_shutdownTimeout).ConfigureAwait(false))
-            {
-                _log.NotAllConnectionsClosedGracefully();
-
-                if (!await ConnectionManager.WalkConnectionsAndAbortAsync(TimeSpan.FromSeconds(1)).ConfigureAwait(false))
-                {
-                    _log.NotAllConnectionsAborted();
-                }
             }
         }
 
@@ -409,6 +383,5 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
             public static readonly Action<object, object> PostCallbackAdapter = (callback, state) => ((Action<T>)callback).Invoke((T)state);
             public static readonly Action<object, object> PostAsyncCallbackAdapter = (callback, state) => ((Action<T>)callback).Invoke((T)state);
         }
-
     }
 }
